e31fec3b003bd65b07c5eb1d99814c7de9faeb00
Justin V. Ledinh <Justin.Vinh.Ledinh@ibm.com>
2017-07-24 23:02:11 -0400

    added app

---
+++ MyApp/app/src/main/java/com/sound_sleuth/jledinh/sound_sleuth/TitleScreen.java
@@ -0,0 +1,13 @@
+package com.sound_sleuth.jledinh.sound_sleuth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TitleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+}
